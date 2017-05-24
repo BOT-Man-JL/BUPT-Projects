20f3b0637b612220fb1670485f243c51fd89f258
BOT Man JL <g123nn@hotmail.com>
2017-05-24 16:16:49 +0800

    Simplify pipe.c;

--- 3-2-Linux/pipe.c
+++ 3-2-Linux/pipe.c
@@ -1,6 +1,6 @@
 
 // An implementation of command
-// $ grep ¨Cv usr < /etc/passwd | wc ¨Cl > result.txt
+// $ grep -v usr < /etc/passwd | wc -l > result.txt
 //
 // By John Lee, 2017/5/22
 
@@ -10,35 +10,22 @@
 #include <unistd.h>  // for fork, dup2, close, execlp
 #include <sys/wait.h>  // for wait
 
-int main (int argc, char *argv[])
+int main ()
 {
     int fd[2], sv;
     pipe (fd);
     if (fork () == 0)
     {
-        int f = open ("/etc/passwd", O_RDONLY);
-        if (f != -1)
-        {
-            dup2 (f, 0);
-            dup2 (fd[1], 1); close (fd[0]); close (fd[1]);
-            execlp ("grep", "grep", "-v", "usr", NULL);
-        }
-        fprintf (stderr, "exec failed: grep\n");
-        exit (1);
+        dup2 (open ("/etc/passwd", O_RDONLY), 0);
+        dup2 (fd[1], 1);
+        close (fd[0]); close (fd[1]);
+        execlp ("grep", "grep", "-v", "usr", NULL);
     }
-    else if (fork () == 0)
+    else
     {
-        int f = open ("result.txt", O_WRONLY | O_CREAT, 0666);
-        if (f != -1)
-        {
-            dup2 (f, 1);
-            dup2 (fd[0], 0); close (fd[0]); close (fd[1]);
-            execlp ("wc", "wc", "-l", NULL);
-        }
-        fprintf (stderr, "exec failed: wc\n");
-        exit (1);
+        dup2 (open ("result.txt", O_WRONLY | O_CREAT, 0666), 1);
+        dup2 (fd[0], 0);
+        close (fd[0]); close (fd[1]);
+        execlp ("wc", "wc", "-l", NULL);
     }
-    close (fd[0]); close (fd[1]);
-    wait (&sv); wait (&sv);
-    return 0;
 }
